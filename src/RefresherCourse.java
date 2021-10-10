4898b87c2f0533e74f2654121bc0b60a7195d668
Luyanda <luyandasmadonsela@gmail.com>
2021-10-10 12:23:09 +0200

    Initial commit

---
+++ src/RefresherCourse.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class RefresherCourse {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World \nWelcome to Java\n24");
+        JOptionPane.showMessageDialog((Component)null, "Satisfied?");
+
+        int i;
+        for(i = 1; i <= 5; ++i) {
+            System.out.println("Welcome to Java");
+        }
+
+        System.out.println("       J        A      V           V      A");
+        System.out.println("       J      A   A      V       V      A   A");
+        System.out.println(" J     J    AAAAAAAAA      V   V      AAAAAAAAA");
+        System.out.println("   J J    A           A      V      A           A");
+
+        for(i = -1; i <= 8; ++i) {
+            System.out.println(i + 1);
+        }
+
+    }
+}
